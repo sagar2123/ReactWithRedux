9e9bc423003e80498a444aabf4a4380a7b1991e2
sagar\sagar_2621 <sagarb@triconinfotech.com>
2018-03-07 10:53:36 +0530

    store added

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,10 +1,37 @@
 import React, {PropTypes} from 'react';
 
 class CoursePage extends React.Component{
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            course: {title: ""}
+        };
+    }
+
+    onTitleChange(event){
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course : course});
+    }
+
+    onClickSave() {
+        alert('this is saved');
+    }
+
     render(){
         return (
             <div>
-                <h1>Cources</h1>
+                <h1>Courses</h1>
+                <h2>Add course</h2>
+                <input 
+                type="text"
+                onChange= {this.onTitleChange.bind(this)}
+                value = {this.state.course.title}/>
+                <input 
+                type="submit"
+                onClick= {this.onClickSave.bind(this)}
+                value = "save"/>
             </div>
         )
     }
