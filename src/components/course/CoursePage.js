618962a1b0c5736dad31acee9ba268540556c0bd
TRICONHOME\sagarb <sagarb@triconinfotech.com>
2018-03-08 01:43:20 +0530

    reducer

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
-
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 class CoursePage extends React.Component{
     constructor(props, context) {
         super(props, context);
@@ -16,13 +17,19 @@ class CoursePage extends React.Component{
     }
 
     onClickSave() {
-        alert('this is saved');
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
     }
 
     render(){
+        debugger;
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add course</h2>
                 <input 
                 type="text"
@@ -37,4 +44,11 @@ class CoursePage extends React.Component{
     }
 }
 
-module.exports = CoursePage;
+function mapStateToProps(state, ownProps) {
+    debugger;
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps)(CoursePage);
